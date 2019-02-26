562e317dfe3228ef6afa413e7c4972dd1e1f55e0
SpencerGilliam <38114786+SpencerGilliam@users.noreply.github.com>
2019-02-25 21:18:11 -0500

    Add files via upload

---
+++ HW5/hw5.py
@@ -0,0 +1,11 @@
+# Spencer Gilliam
+# CS 3560
+# Professor Mourning
+# 02/24/2019
+
+import sys
+
+print("Total Count of Lines, Words, and Characters in the current directory:")
+print("# of Lines: " + sys.argv[-4]) #Outputs the total number of lines in the current directory
+print("# of Words: " + sys.argv[-3]) #Outputs the total number of words in the current directory
+print("# of Characters: " + sys.argv[-2]) #Outputs the total of characters in the current directory
