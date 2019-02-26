c507a3c961d1276b36460e7bd4799a3941a2fe25
SpencerGilliam <38114786+SpencerGilliam@users.noreply.github.com>
2019-02-25 21:15:52 -0500

    Delete hw5.sh

--- hw5.sh
+++
@@ -1,9 +0,0 @@
-# Spencer Gilliam
-# CS 3560
-# Professor Mourning
-# 02/24/2019
-
-
-totalcount=`wc *` #total count command variable 
-
-python hw5.py $totalcount #python script parcing wc command, on piazza it was said it is a valid solution
