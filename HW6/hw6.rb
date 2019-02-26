5ed30fd83feff30348b2be9842a49d835aca01cd
SpencerGilliam <38114786+SpencerGilliam@users.noreply.github.com>
2019-02-25 21:22:04 -0500

    Update hw6.rb

--- HW6/hw6.rb
+++ HW6/hw6.rb
@@ -4,8 +4,8 @@
 # 02/24/2019
 
 
-string = $stdin.read	          	 #reads in string		
-string = string.gsub(/\b[fF]/, "gh")     #changes beginning F chars with gh
+string = gets	          	           #reads in string		
+string = string.gsub(/\b[fF]/, "gh") #changes beginning F chars with gh
 string = string.gsub(/sh\b/, "ti")	 #changes sh chars with ti
 string = string.gsub(/\Bi\B/, "o")	 #changes i chars with o
 puts string	
