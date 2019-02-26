574acdbd0c5dd18c71e89e301136a1328185b380
SpencerGilliam <38114786+SpencerGilliam@users.noreply.github.com>
2019-02-25 21:18:37 -0500

    Add files via upload

---
+++ HW6/hw6.rb
@@ -0,0 +1,11 @@
+# Spencer Gilliam
+# CS 3560
+# Professor Mourning
+# 02/24/2019
+
+
+string = $stdin.read	          	 #reads in string		
+string = string.gsub(/\b[fF]/, "gh")     #changes beginning F chars with gh
+string = string.gsub(/sh\b/, "ti")	 #changes sh chars with ti
+string = string.gsub(/\Bi\B/, "o")	 #changes i chars with o
+puts string	
